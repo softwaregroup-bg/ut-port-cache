48361416b1feb644d9e25f7040cfe1ed363ff346
stamen.peev <stamen.peev@softwaregroup-bg.com>
2019-02-08 12:16:11 +0200

    fix: hardcode namespace to be cache

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ module.exports = ({utPort, registerErrors, utBus}) => class CachePort extends ut
                 engine: require('catbox-memory'), // catbox default engine
                 options: {} // catbox engine options
             },
+            namespace: ['cache'],
             policy: [/* {options, segment}, {options, segment} */] // array of catbox policies
         };
     }
@@ -23,7 +24,7 @@ module.exports = ({utPort, registerErrors, utBus}) => class CachePort extends ut
         this.pull((msg = {}, $meta = {}) => {
             const method = $meta.method;
             if (!method) throw utBus.errors['bus.missingMethod']();
-            const handler = this.methods[method.slice(method.indexOf('.') + 1)];
+            const handler = this.methods[method];
             if (typeof handler !== 'function') throw utBus.errors['bus.methodNotFound']({params: {method}});
             return handler(msg, $meta).catch(e => {
                 throw this.errors.cache(e);
@@ -33,17 +34,17 @@ module.exports = ({utPort, registerErrors, utBus}) => class CachePort extends ut
     }
     handlers() {
         const handlers = {
-            get: ({id, segment}) => this.client.get({id, segment}),
-            set: ({id, segment, value, ttl = 0}) => this.client.set({id, segment}, value, ttl),
-            drop: ({id, segment}) => this.client.drop({id, segment})
+            'cache.get': ({id, segment}) => this.client.get({id, segment}),
+            'cache.set': ({id, segment, value, ttl = 0}) => this.client.set({id, segment}, value, ttl),
+            'cache.drop': ({id, segment}) => this.client.drop({id, segment})
         };
         if (Array.isArray(this.config.policy)) {
             this.config.policy.forEach(policyConfig => {
                 const {options = {}, segment} = policyConfig;
                 const policy = new Catbox.Policy(options, this.client, segment);
-                handlers[`${segment}.get`] = ({id}) => policy.get(id);
-                handlers[`${segment}.set`] = ({id, value, ttl = 0}) => policy.set(id, value, ttl);
-                handlers[`${segment}.drop`] = ({id}) => policy.drop(id);
+                handlers[`cache.${segment}.get`] = ({id}) => policy.get(id);
+                handlers[`cache.${segment}.set`] = ({id, value, ttl = 0}) => policy.set(id, value, ttl);
+                handlers[`cache.${segment}.drop`] = ({id}) => policy.drop(id);
             });
         }
         return handlers;
