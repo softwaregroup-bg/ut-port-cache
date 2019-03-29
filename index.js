98bd669ba725c6f138aafc1de1e6c99a521779a8
stamen.peev <stamen.peev@softwaregroup-bg.com>
2019-03-29 14:11:37 +0200

    feat: implement touch and set

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ module.exports = ({utPort, registerErrors, utBus}) => class CachePort extends ut
         if (!methodName) throw utBus.errors['bus.missingMethod']();
         let cache = $meta && $meta.cache;
         if (!cache) throw this.errors['cachePort.missingCache']({params: {methodName}});
-        if (!['get', 'set', 'drop', 'touch'].includes(cache.operation)) throw this.errors['cachePort.missingOperation']({params: {methodName, operation: cache.operation}});
+        if (!['get', 'set', 'drop', 'testAndSet', 'touch'].includes(cache.operation)) throw this.errors['cachePort.missingOperation']({params: {methodName, operation: cache.operation}});
         let segment = cache.key && cache.key.segment;
         if (!segment) {
             let cacheParams = cache.key && cache.key.params;
@@ -53,14 +53,17 @@ module.exports = ({utPort, registerErrors, utBus}) => class CachePort extends ut
         };
     }
     createPolicy({options, segment}) {
-        const policyOptions = {...options, getDecoratedValue: true};
-        const policy = new Catbox.Policy(policyOptions, this.client, segment);
+        const policy = new Catbox.Policy({
+            ...options,
+            expiresIn: this.config.ttl,
+            getDecoratedValue: true
+        }, this.client, segment);
         return {
             async get({id}) {
                 const result = await policy.get(id);
                 return result.cached === null ? null : result.cached.item;
             },
-            async set({id, value, ttl = this.config.ttl}) {
+            async set({id, value, ttl = 0}) {
                 await policy.set(id, value, ttl);
                 return value;
             },
@@ -68,9 +71,16 @@ module.exports = ({utPort, registerErrors, utBus}) => class CachePort extends ut
                 await policy.drop(id);
                 return null;
             },
-            async touch({id, value}) {
+            async testAndSet({id, value, ttl = 0}) { // set and return previous value
+                const result = await policy.get(id);
+                await policy.set(id, value, ttl);
+                return result.cached === null ? null : result.cached.item;
+            },
+            async touch({id, value, ttl = this.config.ttl}) { // get or set
                 const policy = new Catbox.Policy({
-                    ...policyOptions,
+                    ...options,
+                    expiresIn: ttl,
+                    getDecoratedValue: true,
                     generateTimeout: false,
                     generateFunc: (...params) => typeof value === 'function' ? value(...params) : value
                 }, this.client, segment);
